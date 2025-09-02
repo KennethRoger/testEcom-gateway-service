ff7e15df78f089eb1565b967454b934e006bc24c
KennethRoger <kennethroger11@gmail.com>
2025-09-02 12:17:18 +0530

    Custom gateway service as a substituion for nginx-gateway for more control and scalability

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { validateToken } = require("../controllers/authController");
+
+router.get("/validate", validateToken);
+
+module.exports = router;
