d2b22d2ea3ea82d72fdabbd17469c0a3a4f7ad3d
KennethRoger <kennethroger11@gmail.com>
2025-09-06 18:32:53 +0530

    Added errorhandler middleware and errorObject utility function. Removed controllers, customized some order proxies and added new ones. Separated env for production and development

--- src/SERVICES.js
+++
@@ -1,6 +0,0 @@
-const SERVICES = {
-  user: process.env.USER_SERVICE,
-  order: process.env.ORDER_SERVICE
-}
-
-module.export = SERVICES;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 const express = require("express");
-require("dotenv").config();
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
 
 const createProxy = require("./proxy/createProxy");
-const SERVICES = require("./SERVICES");
 const { authorize } = require("./middlewares/authMiddleware");
-
+const { rateLimit } = require("express-rate-limit");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+});
+
+app.use(limiter);
+
+// SERVICES
+const SERVICES = {
+  user: process.env.USER_SERVICE,
+  order: process.env.ORDER_SERVICE,
+};
 
 // USER
 app.use("/api/users/login", createProxy(SERVICES.user));
@@ -23,10 +38,29 @@ app.use(
 
 // ORDER
 app.use(
+  "/api/order",
+  (req, res, next) => {
+    if (req.method === "GET") {
+      return authorize(["admin"])(req, res, next);
+    }
+    return authorize(["user", "admin"])(req, res, next);
+  },
+  createProxy(SERVICES.order)
+);
+
+app.use(
   "/api/order/",
   authorize(["user", "admin"]),
   createProxy(SERVICES.order)
 );
+
+app.use(
+  "/api/order/status/",
+  authorize(["admin"]),
+  createProxy(SERVICES.order)
+);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Gateway listening on port ${PORT}`);
